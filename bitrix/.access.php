e6ebadf06cb70ba06074be77170d37290efab539
Andryusha <egorov.2001@bk.ru>
2025-01-28 20:56:24 +0500

    Шаблон в папке битрикс и все остальное кроме картинок

---
+++ bitrix/.access.php
@@ -0,0 +1,7 @@
+<?
+$PERM["admin"]["8"]="R";
+$PERM["admin"]["7"]="R";
+$PERM["admin"]["*"]="D";
+$PERM["wizards"]["*"]="D";
+$PERM["routing_index.php"]["2"]="R";
+?>
---
+++ include/logotype.php
@@ -0,0 +1,4 @@
+<picture> 
+	<source srcset="/include/logo.png" media="(max-width:768px)">
+	<img src="/include/лого.png" alt="Логотип Цех Бетонных Изделий">
+</picture> 
