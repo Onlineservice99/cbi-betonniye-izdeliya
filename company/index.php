baa3ba5ac3a6eca7808b172dc6f8e307feac7f68
TomilovVA2003 <104489471+TomilovVA2003@users.noreply.github.com>
2025-03-06 16:13:38 +0300

    Правки 06/03/2025

--- company/index.php
+++ company/index.php
@@ -1,147 +1,151 @@
 <?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 
-$APPLICATION->SetTitle('О компании');
+$APPLICATION->SetTitle("О компании");
 
-?>
-<div class="intec-content">
-    <div class="intec-content-wrapper">
-        <div class="intec-ui-picture">
-            <img src="/images/company.png">
-        </div>
-        <div class="intec-ui-p-t-5 intec-ui-p-b-40" style="border-bottom: 1px solid #F2F2F2">
-            <h2>
-                Интернет-магазин
-            </h2>
-            <p style="line-height: 1.7; color: #808080">
-                Готовое решение INTEC.Universe обладает широкими функциональными возможностями, удобным интерфейсом и гибкими настройками дизайна. Оно позволяет создавать интернет-магазины в соответствии с самыми сложными и изысканными запросами клиентов, под решение обширных маркетинговых и бизнес-задач для любой сферы бизнеса, под товары любой тематики.
-            </p>
-            <div class="intec-ui-p-t-5" style="overflow: hidden">
-                <div class="intec-grid intec-grid-i-h-16 intec-grid-i-v-12 intec-grid-768-wrap">
-                    <div class="intec-grid-item-3 intec-grid-item-768-2 intec-grid-item-500-1">
-                        <div class="intec-grid intec-grid-nowrap intec-grid-a-v-start intec-grid-i-8">
-                            <div class="intec-grid-item-auto intec-ui-picture intec-cl-svg-path-stroke">
-                                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M25 13.3333H32.205C32.8867 13.3333 33.5 13.7483 33.7517 14.3817L36.4283 21.0717C36.585 21.465 36.6667 21.885 36.6667 22.3083V32.2217C36.6667 33.1417 35.92 33.8883 35 33.8883H31.9483" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M26.6667 34.0334H13.6167" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M36.6667 25H30V18.3333H35.3333" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M20.9268 10H23.3335C24.2535 10 25.0002 10.7467 25.0002 11.6667V26.6667H3.3335" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M12.8986 3.41667C9.98026 3.86333 7.5936 6.25 7.14693 9.16833C6.43026 13.8517 10.4069 17.8283 15.0903 17.1117C18.0086 16.665 20.3953 14.2783 20.8419 11.36C21.5603 6.67667 17.5819 2.7 12.8986 3.41667V3.41667Z" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M31.1737 32.1598C32.2047 33.1908 32.2047 34.8623 31.1737 35.8933C30.1428 36.9243 28.4712 36.9243 27.4402 35.8933C26.4092 34.8623 26.4092 33.1908 27.4402 32.1598C28.4712 31.1288 30.1428 31.1288 31.1737 32.1598Z" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M12.8403 32.1598C13.8712 33.1908 13.8712 34.8623 12.8403 35.8933C11.8093 36.9243 10.1377 36.9243 9.10673 35.8933C8.07575 34.8623 8.07575 33.1908 9.10673 32.1598C10.1377 31.1288 11.8093 31.1288 12.8403 32.1598Z" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M8.3335 34.0267H5.00016C4.08016 34.0267 3.3335 33.28 3.3335 32.36V11.6667C3.3335 10.7467 4.08016 10 5.00016 10H7.0635" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M16.6668 10H13.3335V6.93171" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                            </div>
-                            <div class="intec-grid-item">
-                                <div style="line-height: 1.25">
-                                    Быстрая доставка
-                                </div>
-                                <div style="font-size: 14px; line-height: 18px; color: #808080; margin-top: 8px">
-                                    Бережно доставляем товары по России за 24 часа
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="intec-grid-item-3 intec-grid-item-768-2 intec-grid-item-500-1">
-                        <div class="intec-grid intec-grid-nowrap intec-grid-a-v-start intec-grid-i-8">
-                            <div class="intec-grid-item-auto intec-ui-picture intec-cl-svg-path-stroke">
-                                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M31.3717 19.2917L21.3533 33.7634C20.435 35.0884 18.3567 34.44 18.3567 32.8267V23.2884H9.98167C8.655 23.2884 7.87334 21.7984 8.63 20.7067L18.6483 6.23503C19.5667 4.91003 21.645 5.55836 21.645 7.17169V16.71H30.02C31.345 16.71 32.1267 18.2 31.3717 19.2917Z" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                            </div>
-                            <div class="intec-grid-item">
-                                <div style="line-height: 1.25">
-                                    Гарантия качества
-                                </div>
-                                <div style="font-size: 14px; line-height: 18px; color: #808080; margin-top: 8px">
-                                    Соответствуем стандартам и требованиям качества
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="intec-grid-item-3 intec-grid-item-768-2 intec-grid-item-500-1">
-                        <div class="intec-grid intec-grid-nowrap intec-grid-a-v-start intec-grid-i-8">
-                            <div class="intec-grid-item-auto intec-ui-picture intec-cl-svg-path-stroke">
-                                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4.99967 11.6667H3.33301" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M6.66634 6.66667H3.33301" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M28.3333 33.3333H5" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M28.7917 6.66667L27.1484 12.145C26.9367 12.85 26.2867 13.3333 25.5517 13.3333H20.365C19.2484 13.3333 18.4484 12.2567 18.7684 11.1883L20.125 6.66667" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M13.333 21.6667H16.883" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path d="M29.0628 26.6667H11.4161C8.7361 26.6667 6.81443 24.0833 7.58443 21.5167L11.1844 9.51667C11.6928 7.825 13.2494 6.66667 15.0161 6.66667H32.6644C35.3444 6.66667 37.2661 9.25 36.4961 11.8167L32.8961 23.8167C32.3878 25.5083 30.8294 26.6667 29.0628 26.6667Z" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                            </div>
-                            <div class="intec-grid-item">
-                                <div style="line-height: 1.25">
-                                    Гарантия качества
-                                </div>
-                                <div style="font-size: 14px; line-height: 18px; color: #808080; margin-top: 8px">
-                                    Соответствуем стандартам и требованиям качества
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="intec-ui-p-t-5 intec-ui-p-b-20" style="border-bottom: 1px solid #F2F2F2">
-            <h3>
-                Качественный сервис
-            </h3>
-            <ul style="color: #808080; line-height: 1.6;">
-                <li>
-                    Полное погружение в специфику Вашего бизнеса.
-                </li>
-                <li>
-                    Гарантируем индивидуальный подход, высокие результаты и качественное обслуживание.
-                </li>
-                <li>
-                    Персональный менеджер, который всегда держит Вас в курсе работ и консультирует по всем вопросам.
-                </li>
-                <li>
-                    Обучаем Вас и Ваших сотрудников.
-                </li>
-                <li>
-                    Отлаженные бизнес-процессы и четкая организационная среда.
-                </li>
-                <li>
-                    Непрерывно обучаемся и развиваем профессиональные навыки.
-                </li>
-            </ul>
-        </div>
-        <div class="intec-ui-p-t-5 intec-ui-p-b-20" style="border-bottom: 1px solid #F2F2F2">
-            <h3>
-                Творческий подход в дизайне
-            </h3>
-            <p style="line-height: 1.7; color: #808080">
-                Готовое решение INTEC.Universe обладает широкими функциональными возможностями, удобным интерфейсом и гибкими настройками дизайна. Оно позволяет создавать интернет-магазины в соответствии с самыми сложными и изысканными запросами клиентов, под решение обширных маркетинговых и бизнес-задач для любой сферы бизнеса, под товары любой тематики.
-            </p>
-        </div>
-        <div class="intec-ui-p-t-5">
-            <h3>
-                Современные технологии
-            </h3>
-            <ul style="color: #808080; line-height: 1.6;">
-                <li>
-                    Создаем проекты, максимально отвечающие потребностям Вашего бизнеса.
-                </li>
-                <li>
-                    Используем новейшие технологии и современные технологические решения: html5, php7,less, yii2.
-                </li>
-                <li>
-                    Непрерывно повышаем свою квалификацию: изучаем, обучаем, внедряем.
-                </li>
-                <li>
-                    Тщательно тестируем все проекты в Jenkins.
-                </li>
-                <li>
-                    Используем современные системы автоматизации процессов - DashBoard, GIT.
-                </li>
-                <li>
-                    Работаем над проектами по методике Scrum.
-                </li>
-            </ul>
-        </div>
-    </div>
+?><div class="intec-content">
+	<div class="intec-content-wrapper">
+		<div class="intec-ui-picture">
+			<div>
+ <img src="/upload/iblock/2ac/zoi3pr81atdln8hinfebgdjbr7.jpg" style="width:100%;height:auto;"><br>
+			</div>
+		</div>
+		<h2>
+		<p>
+			 О компании ООО “ЦБИ”
+		</p>
+ </h2>
+		<h3>История создания компании</h3>
+		<p>
+			 ООО «ЦБИ» – это молодая компания, официально основанная в феврале 2024 года. Однако её производственная история началась задолго до этого. В 2004 году компания начала свою деятельность с небольшого производства вибролитой тротуарной плитки и садовых бордюров.
+		</p>
+		<p>
+			 За последние 20 лет она динамично развивалась, постепенно расширяя свои возможности и масштабы. Сегодня ООО «ЦБИ» вышла на уровень крупных производителей, специализирующихся на выпуске тротуарной плитки методом вибропрессования, используя передовые технологии, такие как окраска «ColorMix», позволяющая работать с любыми цветовыми палитрами.
+		</p>
+		<hr>
+		<h3>Продукты и услуги</h3>
+		<p>
+			 ООО «ЦБИ» предлагает широкий ассортимент строительных материалов, таких как&nbsp;фасадная плитка&nbsp;и&nbsp;тротуарная плитка,&nbsp;строительные блоки&nbsp;и&nbsp;поребрики, архитектурные изделия из бетона, используя передовые технологии вибролитья и виброштампования. Это позволяет компании удовлетворять не только стандартные, но и индивидуальные запросы клиентов, предлагая решения, которые полностью соответствуют их потребностям.
+		</p>
+		<hr>
+		<h3>
+		<p>
+			 Преимущества компании
+		</p>
+ </h3>
+		<ul>
+			<li>
+			<p>
+				 Использование технологии «ColorMix» при производстве тротуарной плитки. Эта технология позволяет создавать плитку в актуальных пастельных тонах с возможностью комбинирования различных цветовых решений.
+			</p>
+ </li>
+			<li>
+			<p>
+				 Контроль на всех этапах производства, начиная от подбора сырья и заканчивая готовой продукцией, что позволяет гарантировать стабильно высокое качество и долгий срок службы материалов.
+			</p>
+ </li>
+			<li>
+			<p>
+				 Собственное производство дает возможность поддерживать конкурентоспособные цены, сохраняя при этом высокие стандарты. Это делает продукцию доступной для широкого круга клиентов — от частных застройщиков до крупных строительных компаний.
+			</p>
+ </li>
+			<li>
+			<p>
+				 Благодаря развитой логистической системе и наличию продукции на складе, компания обеспечивает быструю отгрузку заказов.
+			</p>
+ </li>
+			<li>
+			<p>
+				 Возможность дозаказа недостающих материалов в штучном варианте, в отличие от конкурентов, которые часто требуют закупку только целыми поддонами. Такой подход не только удобен для клиентов, но и позволяет значительно сэкономить, делая сотрудничество с компанией еще более выгодным и привлекательным.
+			</p>
+ </li>
+			<li>
+			<p>
+				 Сертификация продукции в соответствие с ГОСТ
+			</p>
+ </li>
+		</ul>
+		<div>
+ <br>
+		</div>
+		<hr>
+		<h3>Корпоративная социальная ответственность</h3>
+		 ООО «ЦБИ» заботится о благополучии сотрудников, обеспечивая их униформой, безопасным оборудованием и при необходимости помогая с жильем. Компания поддерживает сплоченность коллектива через регулярные корпоративные мероприятия и тимбилдинг, что создает позитивную атмосферу и укрепляет командный дух. Такой подход повышает мотивацию и формирует сильную, надежную команду.<br>
+ <br>
+		<hr>
+		<h3>Партнеры компании</h3>
+		 ООО «ЦБИ» тесно сотрудничает с рядом ключевых партнеров и поставщиков, что позволяет поддерживать высокий уровень производства и обеспечивать качественную продукцию<br>
+ <br>
+		 Среди основных партнёров компании —&nbsp;Евросинтес,&nbsp;ООО Полимер,&nbsp;ООО СТ Паритет&nbsp;и&nbsp;Рыбинский порт. Эти компании поставляют надёжные и проверенные материалы, а также оборудование, что способствует стабильной работе предприятия и реализации проектов любой сложности.<br>
+ <br>
+		 Сотрудничество с такими ведущими партнёрами позволяет компании использовать современные технологии и материалы, поддерживать высокий стандарт продукции и своевременно выполнять заказы. Надёжные партнёрские отношения играют важную роль в успехе компании и ее способности оставаться конкурентоспособной на рынке.<br>
+ <br>
+		<hr>
+		<p>
+		</p>
+		<h3>Взаимодействие с общественностью и клиентами<br>
+ </h3>
+		 ООО «ЦБИ» активно сотрудничает с клиентами и общественностью, реализуя проекты любого масштаба — от частных заказов до крупных городских объектов.Примером стал проект благоустройства двора ветерана в Рыбинске, выполненный ко Дню Победы, где компания выложила красную брусчатку.<br>
+ <br>
+		<hr>
+	</div>
 </div>
-<?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php') ?>
+<div>
+	<div class="intec-content">
+		<div class="intec-content-wrapper">
+			<h3>Производство</h3>
+			 <?$APPLICATION->IncludeComponent(
+	"bitrix:photo.section",
+	"gallery.default.1",
+	Array(
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"COLUMNS" => "3",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"FIELD_CODE" => array("",""),
+		"FILTER_NAME" => "arrFilter",
+		"IBLOCK_ID" => "37",
+		"IBLOCK_TYPE" => "content",
+		"LAZYLOAD_USE" => "N",
+		"LINE_ELEMENT_COUNT" => "3",
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Фотографии",
+		"PAGE_ELEMENT_COUNT" => "20",
+		"PROPERTY_CODE" => array("",""),
+		"SECTION_CODE" => "",
+		"SECTION_ID" => "58",
+		"SECTION_URL" => "",
+		"SECTION_USER_FIELDS" => array("",""),
+		"SETTINGS_USE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N"
+	)
+);?><br>
+ <br>
+		</div>
+	</div>
+	 &nbsp;
+</div>
+ <br><?php require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php') ?>
